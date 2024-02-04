67890bcfdea7c579bd6cf8a5887d3f7535639ebd
longpanzhou <zhoul83@mcmaster.ca>
2024-02-03 21:24:51 -0500

    Fixed search

--- src/assets/ExtractData.py
+++ src/assets/ExtractData.py
@@ -5,12 +5,12 @@ def main(Filters):
     df = pd.read_csv('2023-10-01-2024-01-29-Middle_East-Israel-Palestine.csv')
 
     def extract_DateTime(DataFrame, DateTime):
-        original_date = datetime.strptime(original_date_str, "%B %d, %Y")
+        original_date = datetime.strptime(DateTime, "%B %d, %Y")
         formatted_date_str = original_date.strftime("%d %B %Y")
         return DataFrame[DataFrame['event_date'] == formatted_date_str]
 
     def extract_Country(DataFrame, Country):
-        if country == 'All': # All, Israel, Palestine
+        if Country == 'All': # All, Israel, Palestine
             return DataFrame
         return dDataFramef[DataFrame['country'] == Country]
         
@@ -41,5 +41,24 @@ def main(Filters):
         elif fatalities == '50+':
             return DataFrame[DataFrame['fatalities'] > 50]
     
+    df = extract_DateTime(df, Filters['DateTime'])
+    df = extract_Country(df, Filters['Country'])
+    df = extract_CiviTarget(df, Filters['CiviTarget'])
+    df = extract_Actor1(df, Filters['Actor1'])
+    df = extract_Event_Type(df, Filters['Event_Type'])
+    df = extract_fatalities(df, Filters['fatalities'])
+
+    return df
+
+def generateData(DataFrame):
+    return DataFrame.to_json(orient='records')
+
 if __name__ == "__main__":
-    main(Filters)
+    test = {
+        'DateTime': 'October 1, 2023',
+        'Country': 'All',
+        'CiviTarget': False,
+        'Actor1': 'All',
+        'Event_Type': 'All',
+        'fatalities': 'All'}
+    main(test)
